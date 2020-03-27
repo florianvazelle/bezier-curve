2065c426cd9cd385b90307e6f6cb3934717cb95d
Glitch (florianvazelle-math-1) <none>
2020-03-27 15:56:05 +0000

    📰🕍 Checkpoint ./script.js:13627699/860:13632248/128:13632532/5

--- script.js
+++ script.js
@@ -26,17 +26,15 @@ var graphics;
 function create() {
     graphics = this.add.graphics();
   
-    var currentCourbe = courbes.data[courbes.index]
-
     for (var i = 0; i < 4; i++) {
-        currentCourbe[i] = []
+        courbes.data[courbes.index].polygonPoints[i] = []
     }
 
     // Points initiaux aux stade 0 => polygone de controle
-    currentCourbe[0][0] = new Phaser.Geom.Point(150, 450);
-    currentCourbe[1][0] = new Phaser.Geom.Point(260, 200);
-    currentCourbe[2][0] = new Phaser.Geom.Point(460, 200);
-    currentCourbe[3][0] = new Phaser.Geom.Point(600, 400);
+    courbes.data[courbes.index].polygonPoints[0][0] = new Phaser.Geom.Point(150, 450);
+    courbes.data[courbes.index].polygonPoints[1][0] = new Phaser.Geom.Point(260, 200);
+    courbes.data[courbes.index].polygonPoints[2][0] = new Phaser.Geom.Point(460, 200);
+    courbes.data[courbes.index].polygonPoints[3][0] = new Phaser.Geom.Point(600, 400);
   
     // A chaque fois que l'on clique 
     this.input.on('pointerdown', function (pointer) {
@@ -46,7 +44,6 @@ function create() {
     
     // A chaque fois que l'on presse une touche
     this.input.keyboard.on('keydown', function (event) {
-      console.log(event)
       if (event.key == "+") {
         pas += 1;
       }
@@ -57,8 +54,13 @@ function create() {
       
       if (event.key == "ArrowLeft") {
         courbes.index = Math.max(courbes.index - 1, 0);
+        console.log(courbes.index)
+        if (courbes.data[courbes.index] == undefined) {
+          courbes.data[courbes.index] = { polygonPoints: [], deCasteljauPoints: [] }
+        }
       }
       if (event.key == "ArrowRight") {
+        console.log(courbes.index)
         courbes.index = Math.min(courbes.index + 1, courbes.data.length);
       }
     }, this);
@@ -74,13 +76,13 @@ function update() {
   
     // Dessine les traits verts
     graphics.lineStyle(2, 0x00ff00);
-    for (var i = 0; i < points.length - 1; i++) {
+    for (var i = 0; i < polygonPoints.length - 1; i++) {
         // Permet de dessiner les lignes entre les points (du stade 0)
         graphics.strokeLineShape(new Phaser.Geom.Line(polygonPoints[i][0].x, polygonPoints[i][0].y, polygonPoints[i + 1][0].x, polygonPoints[i + 1][0].y));
     }
     
     // On update deCasteljauPoints de la courbe courante
-    courbes.data[courbes.index].deCasteljauPoints = deCasteljau(polygonPoints);
+    courbes.data[courbes.index].deCasteljauPoints = deCasteljau(courbes.data[courbes.index].polygonPoints);
     displayDeCasteljau(courbes.data[courbes.index].deCasteljauPoints); 
 }
 
@@ -117,10 +119,13 @@ function deCasteljau(points) {
     return deCasteljauPoints
 }
 
-
+/*
 function deBoor(points) {
   
+    var d = [points[j+point.x - 1]
+  
 }
+*/
 
 /*
 def deBoor(k: int, x: int, t, c, p: int):
