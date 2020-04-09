3b1817d9b8bfa6193792df8b83f52aaf5b4189e8
Glitch (florianvazelle-math-1) <none>
2020-04-09 18:53:35 +0000

    🌋🐯 Checkpoint ./test.js:13627699/46

--- test.js
+++ test.js
@@ -23,8 +23,8 @@ function applyDeBoor(polygonPoints) {
   const n = polygonPoints.length - 1
   const knots = [0, 0, 0, 1, 2 ,2, 2] // length = n + degree + 2
 
-  if (!(degree <= n)) console.log('degree is not <= to n')
-  if (!(knots.length == (n + degree + 2))) console.log('wrong length of knots')
+  if (!(degree <= n)) { console.log('degree is not <= to n'); return [] }
+  if (!(knots.length == (n + degree + 2))) { console.log('wrong length of knots'); return [] }
   
   for (var i = degree; i < n + 1; i += 1) {
     let t = knots[i]
